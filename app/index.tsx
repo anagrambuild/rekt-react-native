1c59fb65f7967a2e872c1d1b7a23155d44ad36f2
Tim Knapp <timknapp12@gmail.com>
2025-06-30 17:38:32 -0600

    navigate to tabs from main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import RektLogo from '@/assets/images/rekt-logo.svg';
 import {
   Column,
@@ -6,15 +8,26 @@ import {
   SignupButton,
   Title1,
 } from '@/components';
+import { useAppContext } from '@/contexts';
+import { router } from 'expo-router';
 
 const Index = () => {
+  const { isLoggedIn, setIsLoggedIn } = useAppContext();
+
+  // Function to check if user is logged in and redirect
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.replace('/(tabs)');
+    }
+  }, [isLoggedIn]);
+
   return (
     <ScreenContainer>
       <RektLogo width={100} height={100} />
       <Title1>Hello World</Title1>
       <Column>
-        <SignupButton onPress={() => console.log('signup')} />
-        <LoginButton onPress={() => console.log('login')} />
+        <SignupButton onPress={() => setIsLoggedIn(true)} />
+        <LoginButton onPress={() => setIsLoggedIn(true)} />
       </Column>
     </ScreenContainer>
   );
