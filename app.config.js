5603b8c835e3070e528263e65d9259ba05078a08
Tim Knapp <timknapp12@gmail.com>
2025-06-30 15:01:55 -0600

    replace app.json with app.config.ts for dynamic functionality

---
+++ app.config.js
@@ -0,0 +1,94 @@
+// Default (development) settings
+let name = 'rekt-react-native';
+let slug = 'rekt-react-native';
+let owner = 'anagram-xyz';
+let version = '0.0.1';
+let easProjectId = 'f2f64b1c-eebc-4198-a0f9-644866b3100e';
+let icon = './assets/images/icon.png';
+let androidIcon = './assets/images/adaptive-icon.png';
+let packageName = 'com.anagramxyz.rektreactnative';
+let bundleIdentifier = 'com.anagramxyz.rektreactnative';
+let env = 'development';
+
+// Preview settings
+if (process.env.DEPLOY_ENVIRONMENT === 'preview') {
+  //   name = 'rekt-preview';
+  //   slug = 'rekt-preview';
+  //   version = '0.0.1-preview';
+  //   easProjectId = 'PREVIEW_PROJECT_ID'; // Replace with actual preview EAS project ID if needed
+  //   icon = './assets/images/preview-icon.png'; // Replace if you have a preview icon
+  //   androidIcon = './assets/images/preview-adaptive-icon.png'; // Replace if you have a preview icon
+  //   packageName = 'com.anagramxyz.rektreactnativepreview';
+  //   bundleIdentifier = 'com.anagramxyz.rektreactnativepreview';
+  //   env = 'preview';
+}
+
+// Example: override for production
+if (process.env.DEPLOY_ENVIRONMENT === 'production') {
+  // name = 'rekt-prod';
+  // slug = 'rekt-prod';
+  // owner = 'anagram-xyz';
+  // version = '1.0.0';
+  // easProjectId = '...';
+  // icon = './assets/images/prod-icon.png';
+  // androidIcon = './assets/images/prod-adaptive-icon.png';
+  // packageName = 'com.anagramxyz.rektreactnativeprod';
+  // bundleIdentifier = 'com.anagramxyz.rektreactnativeprod';
+  // env = 'production';
+}
+
+module.exports = {
+  expo: {
+    name,
+    slug,
+    owner,
+    version,
+    orientation: 'portrait',
+    icon,
+    scheme: 'rektreactnative',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier,
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: androidIcon,
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      package: packageName,
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/rekt-logo.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#000000',
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: easProjectId,
+      },
+      env,
+    },
+  },
+};
