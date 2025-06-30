8f94a4f370df340b9a28ff4ab8bfba36d0d9a904
Tim Knapp <timknapp12@gmail.com>
2025-06-30 11:11:37 -0600

    start with clean slate and add eas.json

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,30 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      'simple-import-sort': require('eslint-plugin-simple-import-sort'),
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            // React and React Native first
+            ['^react$', '^react-native$'],
+            // Other packages
+            ['^@?\\w'],
+            // Internal packages (e.g., @components)
+            ['^@components', '^@?w'],
+            // Parent imports
+            ['^..(?!/?$)', '^../?$'],
+            // Other relative imports
+            ['^./(?=.*/)(?!/?$)', '^.(?!/?$)', '^./?$'],
+            // Style imports
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
   },
 ]);
