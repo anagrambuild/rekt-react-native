0c8041d1b2cac0a27927ff1da1502ae7004ba84a
Tim Knapp <timknapp12@gmail.com>
2025-07-03 16:52:44 -0600

    add back button to trade screen

--- screens/TradeScreen/index.tsx
+++ screens/TradeScreen/index.tsx
@@ -1,20 +1,26 @@
+import { Pressable } from 'react-native';
+
 import { Row, ScreenContainer, Title1 } from '@/components';
 
-import { Stack } from 'expo-router';
+import MaterialIcon from '@expo/vector-icons/MaterialIcons';
+
+import { router, Stack } from 'expo-router';
+import { useTheme } from 'styled-components/native';
+
 export const TradeScreen = () => {
+  const theme = useTheme();
   return (
     <>
-      <Stack.Screen
-        options={{
-          headerShown: true,
-          title: '', // or your custom title
-          //   headerBackTitleVisible: false,
-          // You can customize the header here
-          // tabBarStyle: { display: 'none' }, // Hide tab bar
-        }}
-      />
+      <Stack.Screen options={{ headerShown: false }} />
       <ScreenContainer>
         <Row>
+          <Pressable onPress={() => router.back()}>
+            <MaterialIcon
+              name='keyboard-arrow-left'
+              size={32}
+              color={theme.colors.textSecondary}
+            />
+          </Pressable>
           <Title1>Trade</Title1>
         </Row>
       </ScreenContainer>
