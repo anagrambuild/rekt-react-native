f1d0f81894d00a33769c7b0ab009cb1cfd685b9e
Tim Knapp <timknapp12@gmail.com>
2025-07-08 10:22:00 -0600

    add more token data for prices

--- screens/HomeScreen/homeComponents/PriceChart.tsx
+++ screens/HomeScreen/homeComponents/PriceChart.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { Dimensions } from 'react-native';
+
+import { useHomeContext } from '@/contexts';
 
 // https://gifted-charts.web.app/linechart/#animated
 import { LineChart, yAxisSides } from 'react-native-gifted-charts';
 import styled, { DefaultTheme, useTheme } from 'styled-components/native';
 
-const data = [
+const solData = [
   { value: 167 },
   { value: 168 },
   { value: 169 },
@@ -14,11 +15,44 @@ const data = [
   { value: 171 },
   { value: 171.2 },
 ];
+const ethData = [
+  { value: 2563 },
+  { value: 2565 },
+  { value: 2566 },
+  { value: 2567 },
+  { value: 2568 },
+  { value: 2569 },
+  { value: 2564 },
+];
+const btcData = [
+  { value: 109250 },
+  { value: 109258 },
+  { value: 108966 },
+  { value: 108824 },
+  { value: 108682 },
+  { value: 108640 },
+  { value: 109261 },
+];
 
 export const PriceChart = () => {
   const theme = useTheme();
   const chartWidth = Dimensions.get('window').width * 0.7;
+  const { selectedToken } = useHomeContext();
 
+  const data =
+    selectedToken === 'sol'
+      ? solData
+      : selectedToken === 'eth'
+      ? ethData
+      : btcData;
+
+  const findYAxisOffset = (arr: number[]) => {
+    if (!arr || arr.length === 0) return undefined;
+    return Math.min(...arr);
+  };
+
+  const yAxisOffset = findYAxisOffset(data.map((item) => item.value));
+  console.log('yAxisOffset', yAxisOffset);
   return (
     <Wrapper>
       <LineChart
@@ -26,7 +60,7 @@ export const PriceChart = () => {
         isAnimated
         animationDuration={1200}
         areaChart
-        curved
+        // curved
         color={theme.colors.tint}
         thickness={2}
         startFillColor={theme.colors.tint}
@@ -42,7 +76,7 @@ export const PriceChart = () => {
         xAxisLabelTextStyle={{ color: theme.colors.tint }}
         backgroundColor='transparent'
         initialSpacing={0}
-        yAxisOffset={166} // set to your min value for better axis
+        yAxisOffset={yAxisOffset}
         width={chartWidth}
         yAxisSide={yAxisSides.RIGHT}
       />
--- todo.md
+++ todo.md
@@ -9,7 +9,12 @@ polishing (mismatches from designs)
 
 - flame for splash screen
 - add commas for number strings
+- adjust price chart styles to show wide ranges of numbers and large numbers on y axis like btc prices
 - linear gradient on active tab
 - check with enlarged text and different size devices
 - removed unused colors and theme vars
 - icons for long and short segment control
+
+WIRING UP REAL DATA
+
+- fetch real wallet balance for usdc on HomeContext
