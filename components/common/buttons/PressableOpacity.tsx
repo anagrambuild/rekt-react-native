a310d5572e5ed963f6e3f8ec25f3e7a5fbdeba96
Tim Knapp <timknapp12@gmail.com>
2025-07-08 11:34:03 -0600

    create PressableOpacity component

---
+++ components/common/buttons/PressableOpacity.tsx
@@ -0,0 +1,31 @@
+import { Pressable, PressableProps } from 'react-native';
+
+interface PressableOpacityProps extends PressableProps {
+  loading?: boolean;
+}
+
+export const PressableOpacity = ({
+  children,
+  loading,
+  disabled,
+  style,
+  ...props
+}: PressableOpacityProps) => {
+  return (
+    <Pressable
+      {...props}
+      disabled={disabled || loading}
+      style={(state) => {
+        const baseStyle = typeof style === 'function' ? style(state) : style;
+        return [
+          baseStyle,
+          {
+            opacity: state.pressed && !disabled && !loading ? 0.2 : 1,
+          },
+        ];
+      }}
+    >
+      {children}
+    </Pressable>
+  );
+};
--- components/common/buttons/index.ts
+++ components/common/buttons/index.ts
@@ -1,3 +1,4 @@
 export * from './IconButton';
 export * from './main-buttons';
+export * from './PressableOpacity';
 export * from './SegmentControl';
