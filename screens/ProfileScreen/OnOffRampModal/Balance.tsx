c2f60ccf56756c687bb96888dc18dbc1244d945e
Tim Knapp <timknapp12@gmail.com>
2025-08-14 18:28:02 -0600

    add blue flame

--- screens/ProfileScreen/OnOffRampModal/Balance.tsx
+++ screens/ProfileScreen/OnOffRampModal/Balance.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useRef, useState } from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 
 import coinIcon from '@/assets/images/app-pngs/coin.png';
 import UsdcIcon from '@/assets/images/app-svgs/usdc.svg';
 import WalletSecondaryIcon from '@/assets/images/app-svgs/wallet-secondary.svg';
+import { blueFlameUrl } from '@/assets/videos';
 import {
   BodyMEmphasized,
   BodyMSecondary,
@@ -15,6 +16,7 @@ import {
   IconButton,
   ModalIconButton,
   PressableOpacity,
+  PulsatingContainer,
   Row,
   Title2,
 } from '@/components';
@@ -26,8 +28,11 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 import { EarningsCard } from './EarningsCard';
 import { Image } from 'expo-image';
+import { useVideoPlayer, VideoView } from 'expo-video';
 import { useTranslation } from 'react-i18next';
 import styled, { DefaultTheme, useTheme } from 'styled-components/native';
+
+const videoOffset = Platform.OS === 'ios' ? 38 : 30;
 
 export const Balance = ({
   setView,
@@ -53,6 +58,12 @@ export const Balance = ({
     x: 0,
     y: 0,
   });
+
+  const player = useVideoPlayer(blueFlameUrl, (player) => {
+    player.loop = true;
+    player.play();
+  });
+
   console.log('usdcIconPosition', usdcIconPosition);
   console.log('earningAmountPosition', earningAmountPosition);
   const goToTransfer = () => {
@@ -97,125 +108,154 @@ export const Balance = ({
   }, [usdcIconPosition, earningAmountPosition]);
   console.log('targetPosition', targetPosition);
   return (
-    <Column $gap={24} $alignItems='flex-start' $padding={8}>
-      {/* Header Section */}
-      <Column $justifyContent='space-between' $alignItems='flex-start' $gap={8}>
-        <IconContainer>
-          <WalletSecondaryIcon
-            width={20}
-            height={20}
-            color={theme.colors.textSecondary}
-          />
-        </IconContainer>
-        <Row $justifyContent='space-between'>
-          <Column $alignItems='flex-start' $width='auto'>
-            <BodyMSecondary>{t('Your balance')}</BodyMSecondary>
-            <Row $width='auto' $gap={4} $justifyContent='flex-start'>
-              <Title2>
-                {/* {userData.balance.toLocaleString('en-US', {
+    <Column>
+      {hasBreeze && (
+        <VideoView
+          player={player}
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            bottom: videoOffset,
+            left: 0,
+            backgroundColor: 'transparent',
+          }}
+          pointerEvents='none'
+          nativeControls={false}
+        />
+      )}
+      <Column $gap={24} $alignItems='flex-start' $padding={8}>
+        {/* Header Section */}
+        <Column
+          $justifyContent='space-between'
+          $alignItems='flex-start'
+          $gap={8}
+        >
+          <IconContainer>
+            <WalletSecondaryIcon
+              width={20}
+              height={20}
+              color={theme.colors.textSecondary}
+            />
+          </IconContainer>
+          <Row $justifyContent='space-between'>
+            <Column $alignItems='flex-start' $width='auto'>
+              <BodyMSecondary>{t('Your balance')}</BodyMSecondary>
+              <Row $width='auto' $gap={4} $justifyContent='flex-start'>
+                <Title2>
+                  {/* {userData.balance.toLocaleString('en-US', {
                   minimumFractionDigits: 2,
                   maximumFractionDigits: 2,
                 })} */}
-                18000000000.00
-              </Title2>
-              <View ref={usdcIconRef} onLayout={handleUsdcIconLayout}>
-                <UsdcIcon width={20} height={20} />
-              </View>
-            </Row>
-          </Column>
+                  1800.00
+                </Title2>
+                <View ref={usdcIconRef} onLayout={handleUsdcIconLayout}>
+                  {hasBreeze ? (
+                    <PulsatingContainer duration={1000}>
+                      <UsdcIcon width={20} height={20} />
+                    </PulsatingContainer>
+                  ) : (
+                    <UsdcIcon width={20} height={20} />
+                  )}
+                </View>
+              </Row>
+            </Column>
 
-          {userData.balance > 0 && (
-            <Row $width='auto' $gap={8}>
-              <ModalIconButton
-                onPress={goToWithdraw}
-                icon={
-                  <AntDesign
-                    name='minuscircle'
-                    size={12}
-                    color={theme.colors.textPrimary}
-                  />
-                }
-              >
-                {t('Withdraw')}
-              </ModalIconButton>
-              <IconButton
-                name='history'
-                size={12}
-                color={theme.colors.textPrimary}
-                onPress={handleHistory}
+            {userData.balance > 0 && (
+              <Row $width='auto' $gap={8}>
+                <ModalIconButton
+                  onPress={goToWithdraw}
+                  icon={
+                    <AntDesign
+                      name='minuscircle'
+                      size={12}
+                      color={theme.colors.textPrimary}
+                    />
+                  }
+                >
+                  {t('Withdraw')}
+                </ModalIconButton>
+                <IconButton
+                  name='history'
+                  size={12}
+                  color={theme.colors.textPrimary}
+                  onPress={handleHistory}
+                />
+              </Row>
+            )}
+          </Row>
+        </Column>
+
+        {/* APY Section */}
+        {hasBreeze ? (
+          <EarningsCard
+            targetPosition={targetPosition}
+            handleEarningAmountLayout={handleEarningAmountLayout}
+            earningAmountRef={earningAmountRef}
+            earningAmountTextRef={earningAmountTextRef}
+          />
+        ) : (
+          <Card $padding={16}>
+            <PressableOpacity onPress={goToConfirmBreeze}>
+              <Column $width='auto' $gap={4} $alignItems='flex-start'>
+                <Image source={coinIcon} style={{ width: 64, height: 64 }} />
+                <BodyMEmphasized>
+                  {t('Earn 5% APY on your balance')}
+                </BodyMEmphasized>
+                <BodyMSecondary>
+                  {t('Put your idle dollars to work')}
+                </BodyMSecondary>
+              </Column>
+            </PressableOpacity>
+          </Card>
+        )}
+
+        {/* Deposit Options Section */}
+        <Column $gap={4} $width='100%' $alignItems='flex-start'>
+          <BodyXSMonoSecondary>
+            {t('Deposit funds').toUpperCase()}
+          </BodyXSMonoSecondary>
+
+          <DepositOption onPress={goToTransfer}>
+            <Row $width='auto' $gap={12} $alignItems='center'>
+              <FontAwesome5
+                name='arrow-circle-down'
+                size={24}
+                color={theme.colors.textSecondary}
               />
+              <Column $width='auto' $alignItems='flex-start'>
+                <BodyMEmphasized>{t('Transfer-in')}</BodyMEmphasized>
+                <BodySSecondary>{t('From Solana wallet')}</BodySSecondary>
+              </Column>
             </Row>
-          )}
-        </Row>
-      </Column>
-
-      {/* APY Section */}
-      {hasBreeze ? (
-        <EarningsCard
-          targetPosition={targetPosition}
-          handleEarningAmountLayout={handleEarningAmountLayout}
-          earningAmountRef={earningAmountRef}
-          earningAmountTextRef={earningAmountTextRef}
-        />
-      ) : (
-        <Card $padding={16}>
-          <PressableOpacity onPress={goToConfirmBreeze}>
-            <Column $width='auto' $gap={4} $alignItems='flex-start'>
-              <Image source={coinIcon} style={{ width: 64, height: 64 }} />
-              <BodyMEmphasized>
-                {t('Earn 5% APY on your balance')}
-              </BodyMEmphasized>
-              <BodyMSecondary>
-                {t('Put your idle dollars to work')}
-              </BodyMSecondary>
-            </Column>
-          </PressableOpacity>
-        </Card>
-      )}
-
-      {/* Deposit Options Section */}
-      <Column $gap={4} $width='100%' $alignItems='flex-start'>
-        <BodyXSMonoSecondary>
-          {t('Deposit funds').toUpperCase()}
-        </BodyXSMonoSecondary>
-
-        <DepositOption onPress={goToTransfer}>
-          <Row $width='auto' $gap={12} $alignItems='center'>
-            <FontAwesome5
-              name='arrow-circle-down'
-              size={24}
+            <MaterialIcons
+              name='chevron-right'
+              size={20}
               color={theme.colors.textSecondary}
             />
-            <Column $width='auto' $alignItems='flex-start'>
-              <BodyMEmphasized>{t('Transfer-in')}</BodyMEmphasized>
-              <BodySSecondary>{t('From Solana wallet')}</BodySSecondary>
-            </Column>
-          </Row>
-          <MaterialIcons
-            name='chevron-right'
-            size={20}
-            color={theme.colors.textSecondary}
-          />
-        </DepositOption>
-        <Divider />
-        <DepositOption onPress={goToCard}>
-          <Row $width='auto' $gap={12} $alignItems='center'>
-            <FontAwesome5
-              name='cc-apple-pay'
-              size={24}
+          </DepositOption>
+          <Divider />
+          <DepositOption onPress={goToCard}>
+            <Row $width='auto' $gap={12} $alignItems='center'>
+              <FontAwesome5
+                name='cc-apple-pay'
+                size={24}
+                color={theme.colors.textSecondary}
+              />
+              <Column $width='auto' $alignItems='flex-start'>
+                <BodyMEmphasized>{t('Card or Apple pay')}</BodyMEmphasized>
+                <BodySSecondary>
+                  {t('Instant and secure payment')}
+                </BodySSecondary>
+              </Column>
+            </Row>
+            <MaterialIcons
+              name='chevron-right'
+              size={20}
               color={theme.colors.textSecondary}
             />
-            <Column $width='auto' $alignItems='flex-start'>
-              <BodyMEmphasized>{t('Card or Apple pay')}</BodyMEmphasized>
-              <BodySSecondary>{t('Instant and secure payment')}</BodySSecondary>
-            </Column>
-          </Row>
-          <MaterialIcons
-            name='chevron-right'
-            size={20}
-            color={theme.colors.textSecondary}
-          />
-        </DepositOption>
+          </DepositOption>
+        </Column>
       </Column>
     </Column>
   );
