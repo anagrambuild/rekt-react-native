effcebc17f29b7e01589ad4b6ba581c9d49fc6f2
Tim Knapp <timknapp12@gmail.com>
2025-09-16 10:19:52 -0600

    add secondary icon for segment control

--- components/common/buttons/SegmentControl.tsx
+++ components/common/buttons/SegmentControl.tsx
@@ -5,6 +5,7 @@ import styled, { DefaultTheme, useTheme } from "styled-components/native";
 interface SegmentControlProps {
   Svg: React.ComponentType<any>;
   svgProps?: Record<string, any>;
+  SecondarySvg?: React.ComponentType<any>;
   label?: string;
   selected: boolean;
   onPress?: () => void;
@@ -57,6 +58,7 @@ const SegmentLabel = styled(BodyMSecondary)<{
 export const SegmentControl: React.FC<SegmentControlProps> = ({
   Svg,
   svgProps = {},
+  SecondarySvg,
   label,
   selected,
   onPress,
@@ -96,7 +98,13 @@ export const SegmentControl: React.FC<SegmentControlProps> = ({
         selected && boxShadow ? { boxShadow } : null,
       ]}
     >
-      <Svg color={svgColor} size={20} {...svgProps} />
+      {selected ? (
+        <Svg color={svgColor} size={20} {...svgProps} />
+      ) : (
+        SecondarySvg && (
+          <SecondarySvg color={svgColor} size={20} {...svgProps} />
+        )
+      )}
       {label && <SegmentLabel $selected={selected}>{label}</SegmentLabel>}
     </SegmentButton>
   );
